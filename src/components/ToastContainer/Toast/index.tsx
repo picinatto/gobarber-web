a46902e462965c2e8de8e311dd837d9a52014b78
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-06-11 10:39:07 -0300

    Add the timeout to remove automatically the Toast

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
@@ -11,6 +11,26 @@ interface ToastProps {
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
+
+  // Runs the function when the Toast is created
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    /*
+      Automatically runs the function if the component does not exists anymore
+
+      All functions return from useEffect are automatically run when the
+      component is no longer existent
+
+      So, if the user closes the Toast manually, the timeout is cleared
+    */
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, message.id]);
 
   return (
     <Container type={message.type} hasDescription={!!message.description}>
